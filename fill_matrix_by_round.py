3dbf1319c14c8d79a34019c073079cd60f74d3c4
Tolik Legkyi <>
2022-02-17 14:47:01 +0200

    added comment on top of the matrix

--- fill_matrix_by_round.py
+++ fill_matrix_by_round.py
@@ -1,4 +1,14 @@
-#
+"""
+Result of filled Matrix 7x7:
+
+1  2  3  4  5  6  7
+24 25 26 27 28 29 8
+23 40 41 42 43 30 9
+22 39 48 49 44 31 10
+21 38 47 46 45 32 11
+20 37 36 35 34 33 12
+19 18 17 16 15 14 13
+"""
 cnt = 1
 n = 7
 
@@ -29,6 +39,7 @@ for x in range(n // 2):
         val[n - 1 - i][x] = cnt
         cnt = cnt + 1
 
+# The most center element if it's odd number of N
 if n % 2 == 1:
     val[n//2][n//2] = cnt
 
